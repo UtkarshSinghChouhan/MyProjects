f41341edb37b84380c3957f4c31ed89b0a4d38ee
UtkarshSinghChouhan <uscutkarsh9@gmail.com>
2022-08-03 23:42:51 +0530

    SingleproductPage.html completed

--- Responsive Website/main.js
+++ Responsive Website/main.js
@@ -13,3 +13,24 @@ if(close){
         nav.classList.remove('active');
     })
 }
+
+const pro = Array.from(document.querySelectorAll('.pro'));
+console.log(pro);
+
+for(let i = 0; i < pro.length; i++ ){
+    pro[i].addEventListener('click', () => {
+        window.location.href = 'singleProductPage.html';
+    })
+}
+
+
+
+/*====== SingleProductPage.html =================*/
+var mainImg = document.getElementById('MainImg'),
+smallImg = document.getElementsByClassName('small-img');
+
+for(let i = 0; i < 4; i++){
+smallImg[i].onclick = function(){
+    mainImg.src = smallImg[i].src
+} 
+}
